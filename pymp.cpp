5807c42adcebd7eb061b7b6aeca726f6b0c0b15b
sharpeye <misko.pawel@gmail.com>
2014-07-03 19:15:06 +0400

    performace improved

--- pymp.cpp
+++ pymp.cpp
@@ -1,7 +1,7 @@
 #include <cassert>
-#include <string>
 #include <functional>
-#include <stack>
+#include <string>
+#include <vector>
 #include <boost/python.hpp>
 
 #include <stdint.h>
@@ -9,37 +9,126 @@
 
 namespace py = boost::python;
 
-class Reader
+static bool is_array( cmp_object_t const & obj )
+{
+    switch( obj.type )
+    {
+    case CMP_TYPE_FIXARRAY:
+    case CMP_TYPE_ARRAY16:
+    case CMP_TYPE_ARRAY32:
+        return true;
+    }
+
+    return false;
+}
+
+static bool is_map( cmp_object_t const & obj )
+{
+    switch( obj.type )
+    {
+    case CMP_TYPE_FIXMAP:
+    case CMP_TYPE_MAP16:
+    case CMP_TYPE_MAP32:
+        return true;
+    }
+
+    return false;
+}
+
+class Unpacker
 {
 public:
-    Reader( char const * bytes, size_t len )
+    Unpacker( char const * bytes, size_t len )
         : _ptr( bytes )
         , _end( bytes + len )
     {
-        cmp_init( &_cmp, this, &Reader::read_bytes_, nullptr );
+        _stack.reserve( 512 );
+
+        cmp_init( &_cmp, this, &Unpacker::read_bytes_, nullptr );
     }
 
-    py::object operator() ()
+    py::object unpack()
     {
-        py::object hr;
-
-        _stack.emplace( [&]{
-            hr = read_obj();
-        } );
-
-        while( !_stack.empty() )
+        for( ;; )
         {
-            auto f = std::move( _stack.top() );
-            _stack.pop();
+            if( auto obj = next() )
+            {
+                obj = try_pop( obj );
 
-            f();
+                if( obj )
+                {
+                    return py::object( py::handle<>( obj ) );
+                }
+            }
         }
 
-        return hr;
+        return py::object();
     }
 
 private:
-    py::object read_obj()
+    PyObject * try_pop( PyObject * elem )
+    {
+        while( !_stack.empty() )
+        {
+            auto & top = _stack.back();
+
+            top.append( elem );
+
+            if( !top.complete() )
+            {
+                return nullptr;
+            }
+
+            elem = top.obj;
+
+            _stack.pop_back();
+        }
+
+        return elem;
+    }
+
+    PyObject * next()
+    {
+        auto obj = read_next();
+
+        if( is_array( obj ) )
+        {
+            if( obj.as.array_size )
+            {
+                push_list( obj.as.array_size );
+
+                return nullptr;
+            }
+
+            return PyList_New( 0 );
+        }
+        
+        if( is_map( obj ) )
+        {
+            if( obj.as.map_size )
+            {
+                push_dict( obj.as.map_size );
+
+                return nullptr;
+            }
+
+            return PyDict_New();
+        }
+            
+        return read_simple( obj );
+    }
+
+    void push_list( uint32_t size )
+    {
+        _stack.emplace_back( PyList_New( size ), size, true );
+    }
+
+    void push_dict( uint32_t size )
+    {
+        _stack.emplace_back( PyDict_New(), size * 2, false );
+    }
+
+    cmp_object_t read_next()
     {
         cmp_object_t obj = {};
         if( !cmp_read_object( &_cmp, &obj ) )
@@ -47,59 +136,43 @@ private:
             throw std::runtime_error( std::string( "can't read object: " ) + cmp_strerror( &_cmp) );
         }
 
+        return obj;
+    }
+
+    PyObject * read_simple( cmp_object_t const & obj )
+    {
         switch( obj.type )
         {
         case CMP_TYPE_NIL:
-            return py::object();
+            return py::incref( Py_None );
 
         case CMP_TYPE_BOOLEAN:
-            return py::object( obj.as.boolean );
-
-        case CMP_TYPE_BIN8:
-        case CMP_TYPE_BIN16:
-        case CMP_TYPE_BIN32:
-            assert( 0 );
-            break;
-
-        case CMP_TYPE_EXT8:
-        case CMP_TYPE_EXT16:
-        case CMP_TYPE_EXT32:
-            assert( 0 );
-            break;
+            return py::incref( obj.as.boolean ? Py_True : Py_False );
 
         case CMP_TYPE_FLOAT:
-            return py::object( obj.as.flt );
-
+            return PyFloat_FromDouble( obj.as.flt );
         case CMP_TYPE_DOUBLE:
-            return py::object( obj.as.dbl );
+            return PyFloat_FromDouble( obj.as.dbl );
 
         case CMP_TYPE_POSITIVE_FIXNUM:
         case CMP_TYPE_UINT8:
-            return py::object( +obj.as.u8 );
+            return PyInt_FromLong( +obj.as.u8 );
         case CMP_TYPE_UINT16:
-            return py::object( obj.as.u16 );
+            return PyInt_FromLong( obj.as.u16 );
         case CMP_TYPE_UINT32:
-            return py::object( obj.as.u32 );
+            return PyInt_FromLong( obj.as.u32 );
         case CMP_TYPE_UINT64:
-            return py::object( obj.as.u64 );
+            return PyLong_FromUnsignedLongLong( obj.as.u64 );
 
         case CMP_TYPE_NEGATIVE_FIXNUM:
         case CMP_TYPE_SINT8:
-            return py::object( +obj.as.s8 );
+            return PyInt_FromLong( obj.as.s8 );
         case CMP_TYPE_SINT16:
-            return py::object( obj.as.s16 );
+            return PyInt_FromLong( obj.as.s16 );
         case CMP_TYPE_SINT32:
-            return py::object( obj.as.s32 );
+            return PyInt_FromLong( obj.as.s32 );
         case CMP_TYPE_SINT64:
-            return py::object( obj.as.s64 );
-
-        case CMP_TYPE_FIXEXT1:
-        case CMP_TYPE_FIXEXT2:
-        case CMP_TYPE_FIXEXT4:
-        case CMP_TYPE_FIXEXT8:
-        case CMP_TYPE_FIXEXT16:
-            assert( 0 );
-            break;
+            return PyLong_FromLongLong( obj.as.s64 );
 
         case CMP_TYPE_FIXSTR:
         case CMP_TYPE_STR8:
@@ -110,66 +183,53 @@ private:
         case CMP_TYPE_FIXARRAY:
         case CMP_TYPE_ARRAY16:
         case CMP_TYPE_ARRAY32:
-            return read_array( obj.as.array_size );
 
         case CMP_TYPE_FIXMAP:
         case CMP_TYPE_MAP16:
         case CMP_TYPE_MAP32:
-            return read_map( obj.as.map_size );
+
+        case CMP_TYPE_FIXEXT1:
+        case CMP_TYPE_FIXEXT2:
+        case CMP_TYPE_FIXEXT4:
+        case CMP_TYPE_FIXEXT8:
+        case CMP_TYPE_FIXEXT16:
+
+        case CMP_TYPE_BIN8:
+        case CMP_TYPE_BIN16:
+        case CMP_TYPE_BIN32:
+
+        case CMP_TYPE_EXT8:
+        case CMP_TYPE_EXT16:
+        case CMP_TYPE_EXT32:
+            assert( 0 );
+            break;
         }
 
         throw std::runtime_error( "can't read object: unknown type" );
     }
 
-    py::object read_map( uint32_t n )
+    PyObject * read_str( uint32_t size )
     {
-        py::dict dict;
-
-        for( ; n; --n )
+        if( !size )
         {
-            _stack.emplace( [=]() mutable 
-            {
-                auto key = read_obj(); // read key for one step
-
-                dict[ key ] = read_obj();
-            } );
+            return PyBytes_FromStringAndSize( nullptr, 0 );
         }
 
-        return dict;
-    }
-
-    py::object read_array( uint32_t n )
-    {
-        py::list lst;
-
-        for( ; n; --n )
+        if( static_cast< uint32_t >( _end - _ptr ) < size )
         {
-            _stack.emplace( [=]() mutable
-            { 
-                lst.append( read_obj() );
-            } );
+            throw std::runtime_error( "not enough data" );
         }
 
-        return lst;
-    }
+        auto obj = PyBytes_FromStringAndSize( _ptr, size );
 
-    py::object read_str( uint32_t size )
-    {
-        std::string s;
+        _ptr += size;
 
-        if( size )
-        {
-            s.resize( size );
-
-            read_bytes( &s[ 0 ], s.length() );
-        }
-
-        return py::object( s );
+        return obj;
     }
 
     static bool read_bytes_( cmp_ctx_t * ctx, void * data, size_t limit )
     {
-        auto self = static_cast< Reader * >( ctx->buf );
+        auto self = static_cast< Unpacker * >( ctx->buf );
 
         assert( self && &self->_cmp == ctx );
 
@@ -190,13 +250,70 @@ private:
     }
     
 private:
-    std::stack< std::function< void () > > _stack;
+    struct Item
+    {
+        PyObject * obj;
+        PyObject * key;
+        uint32_t size;
+        uint32_t index;
+        bool is_list;
+
+        Item( PyObject * obj, uint32_t size, bool is_list )
+            : obj( obj )
+            , size( size )
+            , is_list( is_list )
+            , index()
+            , key()
+        {}
+
+        bool complete()
+        {
+            return index == size;
+        }
+
+        void append( PyObject * item )
+        {
+            if( append_impl( item ) )
+            {
+                py::throw_error_already_set();
+            }
+
+            ++index;
+        }
+
+        int append_impl( PyObject * elem )
+        {
+            if( is_list )
+            {
+                return PyList_SetItem( obj, index, elem );
+            }
+           
+            if( index % 2 == 1 )
+            {
+                auto hr = PyDict_SetItem( obj, key, elem );
+
+                py::decref( key );
+                py::decref( elem );
+
+                key = nullptr;
+
+                return hr;
+            }
+
+            key = elem;
+
+            return 0;
+        }
+
+    }; // Item
+
+    std::vector< Item > _stack;
     cmp_ctx_t _cmp;
 
     char const * _ptr;
     char const * _end;
 
-}; // Reader
+}; // Unpacker
 
 static py::object unpack( py::object bytes )
 {
@@ -210,16 +327,19 @@ static py::object unpack( py::object bytes )
     const void * data = nullptr;
     Py_ssize_t size = 0;
 
-    PyObject_AsReadBuffer( p, &data, &size );
+    if( PyObject_AsReadBuffer( p, &data, &size ) )
+    {
+        py::throw_error_already_set();
+    }
 
     if( !data || !size )
     {
         throw std::runtime_error( "can't unpack: no data" );
     }
 
-    Reader r( reinterpret_cast< char const * >( data ), size );
+    Unpacker unpacker( reinterpret_cast< char const * >( data ), size );
 
-    return r();
+    return unpacker.unpack();
 }
 
 BOOST_PYTHON_MODULE( pymp )
